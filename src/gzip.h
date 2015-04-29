453f1d15910267235bac6223f834bfe1ba90fe47
masukawa <masukawa@sor.westlab>
2015-04-29 16:27:02 +0900

    7 files fix

--- src/gzip.h
+++ src/gzip.h
@@ -10,8 +10,8 @@
  * $Id: Gzip.H,v 5.2 2011-11-16 05:59:23 sin Exp $
 */
 
-#ifndef Gzip_H
-#define Gzip_H
+#ifndef GZIP_H_
+#define GZIP_H_
 
 
 #define FILEBUFSIZ   1024 * 1024         /* 入力バッファサイズ（任意） */
@@ -20,8 +20,8 @@
 #define TMPBUFSIZ 200
 
 /* gzip flag byte */
-#define GZ_MAGIC_0	0x1f
-#define GZ_MAGIC_1	0x8b
+#define GZ_MAGIC_0   0x1f
+#define GZ_MAGIC_1   0x8b
 #define ASCII_FLAG   0x01 /* bit 0 set: file probably ascii text */
 #define HEAD_CRC     0x02 /* bit 1 set: header CRC present */
 #define EXTRA_FIELD  0x04 /* bit 2 set: extra field present */
@@ -30,17 +30,16 @@
 #define RESERVED     0xE0 /* bits 5..7: reserved */
 
 class Gzip{
-    public:
-        u_char outbuffer[SAVE_MAX_SIZE];
-        Gzip();
-        ~Gzip();
-        int dec_init(z_stream *z);
-        int dec_gzip(u_char* outbuf, u_char* inbuf, int len, z_stream *z);
-        int dec_zlib(u_char* outbuf, u_char* inbuf, int len, z_stream *z);
-        int dec_deflate(u_char* outbuf, u_char* inbuf, int size, z_stream *z);
+ public:
+  u_char outbuffer[SAVE_MAX_SIZE];
+  Gzip();
+  ~Gzip();
+  int dec_init(z_stream *z);
+  int dec_gzip(u_char* outbuf, u_char* inbuf, int len, z_stream *z);
+  int dec_zlib(u_char* outbuf, u_char* inbuf, int len, z_stream *z);
+  int dec_deflate(u_char* outbuf, u_char* inbuf, int size, z_stream *z);
 
-    private:
-
+ private:
 };
 
-#endif
+#endif  // GZIP_H_
